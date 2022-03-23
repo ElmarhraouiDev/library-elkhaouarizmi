551740401923c342338698808f7900673d0a381e
ayoub <elmarhraoui.ayoub@gmail.com>
2022-03-23 02:20:59 +0100

    update

--- application/controllers/Bookissue.php
+++ application/controllers/Bookissue.php
@@ -117,10 +117,10 @@ class Bookissue extends Admin_Controller
             $member = $this->member_m->get_single_member(array('code' => $memberCode));
             if (calculate($member)) {
                 print_r("ok1");
-
+                $this->data['book'] = $this->book_m->get_book();
                 // $this->data['member']       = $member;
                 // $this->data['bookcategory'] = pluck($this->bookcategory_m->get_bookcategory(), 'name', 'bookcategoryID');
-                $this->data['book']         = pluck($this->book_m->get_book(), 'name', 'bookID');
+                // $this->data['book']         = pluck($this->book_m->get_book(), 'name', 'bookID');
                 // $this->data['bookissues']   = $this->bookissue_m->get_order_by_bookissue(['deleted_at' => 0, 'memberID' => $memberCode]);
                 // $this->data['role']         = $this->role_m->get_single_role(array('roleID' => $member->roleID));
                 // $this->data['classe']       = $this->classe_m->get_single_classe(array('classeID' => $member->classeID));
