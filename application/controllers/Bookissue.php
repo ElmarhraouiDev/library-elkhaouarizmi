cb57ac8250874a8ee5e2445f1c586d191cf94676
ayoub <elmarhraoui.ayoub@gmail.com>
2022-03-23 02:17:49 +0100

    update

--- application/controllers/Bookissue.php
+++ application/controllers/Bookissue.php
@@ -110,16 +110,17 @@ class Bookissue extends Admin_Controller
     {
         $data['success'] = 0;
         $data['books'] = [];
-
+        $data['book']= [];
         $books = array();
         $memberCode = htmlentities(escapeString($this->uri->segment(3)));
         if ((int) $memberCode) {
-            print_r("ok1");
             $member = $this->member_m->get_single_member(array('code' => $memberCode));
             if (calculate($member)) {
+                print_r("ok1");
+
                 // $this->data['member']       = $member;
                 // $this->data['bookcategory'] = pluck($this->bookcategory_m->get_bookcategory(), 'name', 'bookcategoryID');
-                // $this->data['book']         = pluck($this->book_m->get_book(), 'name', 'bookID');
+                $this->data['book']         = pluck($this->book_m->get_book(), 'name', 'bookID');
                 // $this->data['bookissues']   = $this->bookissue_m->get_order_by_bookissue(['deleted_at' => 0, 'memberID' => $memberCode]);
                 // $this->data['role']         = $this->role_m->get_single_role(array('roleID' => $member->roleID));
                 // $this->data['classe']       = $this->classe_m->get_single_classe(array('classeID' => $member->classeID));
@@ -147,7 +148,6 @@ class Bookissue extends Admin_Controller
                 $data['books'] = $books;
                 die(json_encode($data));
             } else {
-                print_r("ok2");
                 $data['success'] = 1;
                 die(json_encode($data));
             }
