75582fcb43883511ced1bc449444b75515caf010
ayoub <elmarhraoui.ayoub@gmail.com>
2022-03-29 22:15:45 +0000

    update

--- application/controllers/Bookissue.php
+++ application/controllers/Bookissue.php
@@ -197,6 +197,7 @@ class Bookissue extends Admin_Controller
             foreach($bookCodes as $bookcode) {
                 $arrayBookcode = explode('-', $bookcode);
                 $libraryconfigure = $this->libraryconfigure_m->get_single_libraryconfigure(array('roleID' => $roleID,'booktype' => $bookcode->booktypeID));
+                print("ok");
                 print_r($libraryconfigure);
                 die;
                 if (empty($libraryconfigure)) {
